2070a1fb38e0a6fa489fece5c4efd82ea2eb0d70
Peter Zhou <zetapow@gmail.com>
2025-06-23 11:55:30 +1200

    backend marker rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Test from "./components/Test";
 import GeoSearch from "./components/GeoSearch";
+import StationMap from "./components/StationMap";
 
 function App() {
   return (
     <>
       <h1>Welcome to Mission 5</h1>
       <GeoSearch />
+      <StationMap />
       <Test />
     </>
   );
