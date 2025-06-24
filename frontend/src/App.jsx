a879542c81efe09de22f94b77667623d35a11d7c
Eugene Lee <eugenel@missionreadyhq.com>
2025-06-24 16:22:23 +1200

    Header done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Test from "./components/Test";
 import GeoSearch from "./components/GeoSearch";
+import Header from "./components/shared/Header/Header";
 
 function App() {
   return (
     <>
       <h1>Welcome to Mission 5</h1>
+      <Header />
       <GeoSearch />
       <Test />
     </>
